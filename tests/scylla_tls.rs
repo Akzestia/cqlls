8233ec34d666b1768e2d512964ff6035f649239b
アクゼスティア <akzestia@gmail.com>
2026-01-29 17:01:22 +0900

    Update scylla_tls.rs

--- tests/scylla_tls.rs
+++ tests/scylla_tls.rs
@@ -1,4 +1,4 @@
-use cql_lsp::cqlsh::{CqlSettings, check_connection, query_keyspaces};
+use cqlls::cqlsh::{CqlSettings, check_connection, query_keyspaces};
 
 const SCYLLA_HOST: &str = "127.0.0.1:9042";
 
@@ -21,9 +21,7 @@ async fn test_connection_with_tls() {
 
 #[tokio::test]
 async fn test_query_data() {
-    let ca_cert_file = std::env::var("CQL_LSP_TLS_CA_CERT_FILE").unwrap_or_else(|_| "".to_string());
-    let config =
-        CqlSettings::from_env(SCYLLA_HOST, "cassandra", "cassandra").with_tls(ca_cert_file);
+    let config = CqlSettings::new().with_tls("./certs/ca.crt");
     let result = check_connection(&config).await;
     assert!(result.is_ok(), "Failed to connect: {:?}", result.err());
 
