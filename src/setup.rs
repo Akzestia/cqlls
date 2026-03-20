0cf084d67cc0d7a60a9ab1601ced1344b64e952e
アクゼスティア <akzestia@gmail.com>
2026-03-20 13:45:47 +0900

    Update log dir path, from cql_lsp/output.log to cqlls/cqlls.log

--- src/setup.rs
+++ src/setup.rs
@@ -1,26 +1,9 @@
 /*
-MIT License
+    MIT License
 
-Copyright (c) 2025-2026 アクゼスティア
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+    Copyright (c) 2026 アクゼスティア
 */
+
 use dirs::data_dir;
 use std::{fs::File, io::Write, path::PathBuf};
 
@@ -37,9 +20,9 @@ pub struct SetupConfig {
 
 pub fn setup_logger() -> Result<(), fern::InitError> {
     let mut log_path = data_dir().unwrap_or_else(|| PathBuf::from("."));
-    log_path.push("cql_lsp");
+    log_path.push("cqlls");
     std::fs::create_dir_all(&log_path).expect("Failed to create log directory");
-    log_path.push("output.log");
+    log_path.push("cqlls.log");
 
     fern::Dispatch::new()
         .format(|out, message, record| {
@@ -61,7 +44,7 @@ pub fn setup_logger() -> Result<(), fern::InitError> {
 
 pub fn setup_config() -> Result<(), Box<dyn std::error::Error>> {
     let mut config_path = data_dir().unwrap_or_else(|| PathBuf::from("."));
-    config_path.push("cql_lsp/config.lsp");
+    config_path.push("cqlls/config.lsp");
     println!("Config: {:?}", config_path.to_str());
 
     if !config_path.exists() {
@@ -70,8 +53,7 @@ pub fn setup_config() -> Result<(), Box<dyn std::error::Error>> {
 
         write!(
             file,
-            "[db_context]\npassword = \"{}\"\nuser = \"{}\"\nip = \"{}\"\n",
-            "cassandara", "cassandara", "127.0.0.1"
+            "[db_context]\npassword = \"cassandara\"\nuser = \"cassandara\"\nip = \"127.0.0.1\"\n"
         )?;
 
         return Ok(());
