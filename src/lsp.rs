939e8aa360dd10c5adee93d8b444ffe5f3aeaa5b
アクゼスティア <akzestia@gmail.com>
2026-03-20 13:44:19 +0900

    Cleanup imports and update LICENSE

--- src/lsp.rs
+++ src/lsp.rs
@@ -1,47 +1,14 @@
 /*
-MIT License
+    MIT License
 
-Copyright (c) 2025-2026 アクゼスティア
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+    Copyright (c) 2026 アクゼスティア
 */
-
-use tower_lsp::lsp_types::*;
-use tower_lsp::{Client, LanguageServer};
-
-use std::collections::HashMap;
-use tokio::sync::RwLock;
 
 use crate::cqlsh::CqlSettings;
-
-/*
-    Based on DataStax HCD && CQL versions 3.4+
-
-    HCD
-    https://docs.datastax.com/en/cql/hcd/reference/cql-reference-about.html
-    CQL
-    https://cassandra.apache.org/doc/latest/cassandra/developing/cql/cql_singlefile.html
-
-    Note!
-
-    Some of the default CQL functions will be different because of DataStax HCD extensions
-*/
+use std::collections::HashMap;
+use tokio::sync::RwLock;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer};
 
 #[derive(Debug)]
 pub struct FormattingSettings {
