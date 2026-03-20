f3db8294d68b99db20e97b3647e900171799728a
アクゼスティア <akzestia@gmail.com>
2026-03-20 13:44:34 +0900

    Cleanup imports and update LICENSE

--- src/cqlsh.rs
+++ src/cqlsh.rs
@@ -1,26 +1,9 @@
 /*
-MIT License
+    MIT License
 
-Copyright (c) 2025-2026 アクゼスティア
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+    Copyright (c) 2026 アクゼスティア
 */
+
 use futures::stream::StreamExt;
 use scylla::{
     DeserializeRow,
@@ -34,9 +17,6 @@ use std::time::Duration;
 use log::info;
 use rustls::pki_types::{CertificateDer, pem::PemObject};
 use rustls::{ClientConfig, RootCertStore};
-use rustls_pemfile;
-use std::fs::File;
-use std::io::BufReader;
 use std::sync::Arc;
 
 #[derive(DeserializeRow)]
