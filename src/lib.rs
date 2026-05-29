3019a2887b02f92351fcef15c9200be1acca9abb
アクゼスティア <akzestia@gmail.com>
2026-05-29 15:18:38 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 pub mod completions;
+pub mod config;
 pub mod consts;
 pub mod cqlsh;
 pub mod diagnostics;
 pub mod formatting;
 pub mod handlers;
+pub mod logger;
 pub mod lsp;
-pub mod setup;
+#[cfg(any(test, debug_assertions))]
+pub mod test_base;
 pub mod tree_sitter;
 pub mod utils;
 pub mod version;
