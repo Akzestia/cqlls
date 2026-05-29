cd4068d12d0c531c4dbc5cb42f33ba719f7a1803
アクゼスティア <akzestia@gmail.com>
2026-05-29 14:21:46 +0900

    Update version to 4.1.0

--- src/version.rs
+++ src/version.rs
@@ -5,8 +5,8 @@
 */
 
 const VERSION_MAJOR: u8 = 4;
-const VERSION_MINOR: u8 = 0;
-const VERSION_PATCH: u8 = 1;
+const VERSION_MINOR: u8 = 1;
+const VERSION_PATCH: u8 = 0;
 
 pub fn version() -> String {
     format!("cqlls v{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}")
